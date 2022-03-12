511c0f58c85ad37e3b2dfcc8855211adb55477aa
Bùi Hà Nhi <buihanhi.dev@gmail.com>
2022-03-13 00:08:50 +0700

    update

---
+++ frontend/src/component/layout/Not Found/NotFound.js
@@ -0,0 +1,18 @@
+/* import React from "react";
+import NotFoundImage from "../../../images/catSee.png";
+import "./NotFound.css";
+import { Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div className="PageNotFound">
+      <img src={NotFoundImage} />
+      <Typography>Page Not Found!</Typography>
+      <Link to="/">Back to Homepage</Link>
+    </div>
+  );
+};
+
+export default NotFound;
+ */
